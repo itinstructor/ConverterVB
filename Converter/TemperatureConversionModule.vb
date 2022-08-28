f852831c4d8a83b721afeb367c7691e5422d6c43
itinstructor <32806514+itinstructor@users.noreply.github.com>
2022-08-28 17:22:55 -0600

    Initial Commit

---
+++ Converter/TemperatureConversionModule.vb
@@ -0,0 +1,43 @@
+﻿Module TemperatureConversionModule
+    Friend Sub TemperatureConversion()
+
+        ' Declare local variables to store the input and result
+        Dim decInput As Decimal
+        Dim decResult As Decimal
+
+        ' Set the result labels
+        frmMainForm.lblTemperatureFrom.Text = frmMainForm.lstTemperatureFrom.Text
+        frmMainForm.lblTemperatureTo.Text = frmMainForm.lstTemperatureTo.Text
+        Try
+            ' Get the number to be converted
+            decInput = Decimal.Parse(frmMainForm.txtTemperatureFrom.Text)
+
+            If (frmMainForm.lstTemperatureFrom.Text = "Fahrenheit") Then
+                If (frmMainForm.lstTemperatureTo.Text = "Celsius") Then
+                    decResult = ((decInput - 32) * 5) / 9
+                ElseIf (frmMainForm.lstTemperatureTo.Text = "Fahrenheit") Then
+                    decResult = decInput
+                End If
+            ElseIf (frmMainForm.lstTemperatureFrom.Text = "Celsius") Then
+                If (frmMainForm.lstTemperatureTo.Text = "Celsius") Then
+                    decResult = decInput
+                ElseIf (frmMainForm.lstTemperatureTo.Text = "Fahrenheit") Then
+                    decResult = ((decInput * 9) / 5) + 32
+                End If
+            End If
+
+            ' Convert the result to a string, round it off, and add the degree character
+            frmMainForm.txtTemperatureTo.Text = RemoveTrailingZeros(decResult.ToString("n" & frmMainForm.strDecimalPlaces)) & Chr(176)
+            frmMainForm.txtTemperatureFrom.Focus()
+            frmMainForm.txtTemperatureFrom.SelectAll()
+
+            ' Handle any exceptions
+        Catch ex As Exception
+            MessageBox.Show("Please enter a valid number.", "Entry error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            With frmMainForm.txtTemperatureFrom
+                .Clear()
+                .Focus()
+            End With
+        End Try
+    End Sub
+End Module
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ConverterVB
-A measurement converter in Visual Basic
+# Measurement Converter in Visual Basic
+
+A measurement converter in Visual Basic.
+
+
+### License
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+
+Copyright (c) 2022 William A Loring
